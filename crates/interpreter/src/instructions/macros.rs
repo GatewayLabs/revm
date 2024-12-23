c469f550335a01b8c07078172095e6504fec53da
Lucas (Kbooz) <lucas1inacio7souza@gmail.com>
2024-12-23 16:16:35 -0300

    feat: created pop_top_gates macro

--- crates/interpreter/src/instructions/macros.rs
+++ crates/interpreter/src/instructions/macros.rs
@@ -250,6 +250,51 @@ macro_rules! pop_top {
     };
 }
 
+// Pops 'StackValueData' and 'GateIndexVec' values from the stack. Fails the instruction if the stack is too small.
+#[macro_export]
+macro_rules! pop_top_gates {
+    ($interp:expr, $x1:ident, $garbled_x1:ident) => {
+        if $interp.stack.len() < 1 {
+            $interp.instruction_result = $crate::InstructionResult::StackUnderflow;
+            return;
+        }
+        // SAFETY: Length is checked above.
+        let mut $x1 = unsafe { $interp.stack.top_unsafe() };
+        let $garbled_x1 = $x1.to_garbled_value(&mut $interp.circuit_builder);
+    };
+    ($interp:expr, $x1:ident, $x2:ident, $garbled_x1:ident, $garbled_x2:ident) => {
+        if $interp.stack.len() < 2 {
+            $interp.instruction_result = $crate::InstructionResult::StackUnderflow;
+            return;
+        }
+        // SAFETY: Length is checked above.
+        let ($x1, $x2, $garbled_x1, $garbled_x2) = unsafe {
+            let (val1, val2) = $interp.stack.pop_top_unsafe();
+            let (garbled_val1, garbled_val2) = (
+                val1.to_garbled_value(&mut $interp.circuit_builder),
+                val2.to_garbled_value(&mut $interp.circuit_builder),
+            );
+            (val1, val2, garbled_val1, garbled_val2)
+        };
+    };
+    ($interp:expr, $x1:ident, $x2:ident, $x3:ident) => {
+        if ($interp.stack.len() < 3) {
+            $interp.instruction_result = $crate::InstructionResult::StackUnderflow;
+            return;
+        }
+        // SAFETY: Length is checked above.
+        let ($x1, $x2, $x3, $garbled_x1, $garbled_x2, $garbled_x3) = unsafe {
+            let (val1, val2, val3) = $interp.stack.pop_top_unsafe();
+            let (garbled_val1, garbled_val2, garbled_val3) = (
+                val1.to_garbled_value(&mut $interp.circuit_builder),
+                val2.to_garbled_value(&mut $interp.circuit_builder),
+                val3.to_garbled_value(&mut $interp.circuit_builder),
+            );
+            (val1, val2, garbled_val1, garbled_val2, garbled_val3)
+        };
+    };
+}
+
 /// Pushes `B256` values onto the stack. Fails the instruction if the stack is full.
 #[macro_export]
 macro_rules! push_b256 {
