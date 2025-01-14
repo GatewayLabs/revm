f5d9e813ffd61ee0a3e6233d52e4364617ad6811
João Conserva <boscoj@outlook.com>
2025-01-14 17:57:48 -0300

    feat: add conversion functions for GarbledUint and Uint types in utility module

--- crates/interpreter/src/instructions/utility.rs
+++ crates/interpreter/src/instructions/utility.rs
@@ -19,7 +19,35 @@ pub(crate) unsafe fn read_u16(ptr: *const u8) -> u16 {
     u16::from_be_bytes(core::slice::from_raw_parts(ptr, 2).try_into().unwrap())
 }
 
-pub fn ruint_to_garbled_uint(value: &Uint<256, 4>) -> GarbledUint<64> {
+pub fn ruint_to_garbled_uint(value: &Uint<256, 4>) -> GarbledUint<256> {
+    let bytes: [u8; 32] = value.to_le_bytes();
+
+    let bits: Vec<bool> = bytes
+        .iter()
+        .flat_map(|&byte| (0..8).map(move |i| ((byte >> i) & 1) == 1))
+        .collect();
+
+    GarbledUint::<256>::new(bits)
+}
+
+pub fn garbled_uint_to_ruint(value: &GarbledUint<256>) -> Uint<256, 4> {
+    let bytes: Vec<u8> = value
+        .bits
+        .chunks(8)
+        .map(|chunk| {
+            chunk
+                .iter()
+                .enumerate()
+                .fold(0, |byte, (i, &bit)| byte | ((bit as u8) << i))
+        })
+        .collect();
+
+    let mut array = [0u8; 32];
+    array.copy_from_slice(&bytes);
+    Uint::from_le_bytes(array)
+}
+
+pub fn ruint_to_garbled_uint64(value: &Uint<256, 4>) -> GarbledUint<64> {
     let bytes: [u8; 32] = value.to_le_bytes();
     
     let mut bits = Vec::with_capacity(64);
@@ -32,7 +60,7 @@ pub fn ruint_to_garbled_uint(value: &Uint<256, 4>) -> GarbledUint<64> {
     GarbledUint::<64>::new(bits)
 }
 
-pub fn garbled_uint_to_ruint<const N: usize>(value: &GarbledUint<N>) -> Uint<256, 4> {
+pub fn garbled_uint64_to_ruint<const N: usize>(value: &GarbledUint<N>) -> Uint<256, 4> {
     // Ensure we don't exceed the maximum number of chunks
     let chunk_count = (value.bits.len() + 7) / 8;
     let bytes: Vec<u8> = value
